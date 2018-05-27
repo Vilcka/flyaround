774fc054f982395dcf3b6dc6fca5a8e769c5bdcd
Vilcka <vilc86@hotmail.com>
2018-05-27 06:30:16 +0200

    webpack encore

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('web/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('style', './assets/scss/main.scss')
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSassLoader()
+    .autoProvideVariables({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+    })
+;
+module.exports = Encore.getWebpackConfig();
